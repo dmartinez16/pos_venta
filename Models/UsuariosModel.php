4b0697b337cb6133051c34afbcccfdac6b4fbfc7
dmartinez16 <chingasilvaet@gmail.com>
2022-11-23 20:36:48 -0500

    Conexión a base de datos

---
+++ Models/UsuariosModel.php
@@ -0,0 +1,13 @@
+<?php 
+class UsuariosModel extends Query{
+    public function __construct(){
+      parent::__construct();
+    }
+    public function getUsuario(){
+        $sql = "SELECT * FROM usuario";
+        $data = $this->select($sql);
+        return $data;
+    }
+}
+
+?>
